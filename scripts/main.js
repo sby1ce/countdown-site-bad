0b534a51d1a00bf37df4b2fc568e26a9f1144959
Vitavisu <vitavisu@gmail.com>
2023-07-25 16:48:54 +0300

    Stable, put timers into a dict

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
-function convertDateToString(interval, format) {
-  const timeUnits = [
-    { key: 'weeks', divisor: 1000 * 60 * 60 * 24 * 7, suffix: 'w' },
-    { key: 'days', divisor: 1000 * 60 * 60 * 24, suffix: 'd' },
-    { key: 'hours', divisor: 1000 * 60 * 60, suffix: 'h' },
-    { key: 'minutes', divisor: 1000 * 60, suffix: 'm' },
-    { key: 'seconds', divisor: 1000, suffix: 's' },
-    { key: 'milliseconds', divisor: 1, suffix: 'ms' }
-  ];
+const timeUnits = [
+  { key: 'weeks', divisor: 1000 * 60 * 60 * 24 * 7, suffix: 'w' },
+  { key: 'days', divisor: 1000 * 60 * 60 * 24, suffix: 'd' },
+  { key: 'hours', divisor: 1000 * 60 * 60, suffix: 'h' },
+  { key: 'minutes', divisor: 1000 * 60, suffix: 'm' },
+  { key: 'seconds', divisor: 1000, suffix: 's' },
+  { key: 'milliseconds', divisor: 1, suffix: 'ms' }
+];
+let timers = {
+  'taxiDeparture': new Date("Jul 28, 2023 10:30:00 UTC+0"),
+  'trainDeparture': new Date("Jul 28, 2023 13:12:00 UTC+0"),
+  'trainArrival': new Date("Jul 29, 2023 20:56:00 UTC+0"),
+  'taxiArrival': new Date("Jul 29, 2023 21:56:00 UTC+0"),
+  'start': new Date("2023-07-17T19:00:00Z"),
+  'test': new Date("2023-07-17T21:00:00Z")
+}
 
+const dhms = {'days': true, 'hours': true, 'minutes': true, 'seconds': true};
+
+function convertDateToString(interval, format) {
   const result = timeUnits.reduce((acc, unit) => {
     if (format[unit.key]) {
       const value = Math.abs(Math.floor(interval / unit.divisor));
@@ -20,22 +30,9 @@ function convertDateToString(interval, format) {
   return interval < 0 ? `-${result}` : result.trim();
 }
 
-
 function writeTimer(countdownElement) {
   // Set the date we're counting down to
-  if (countdownElement === "trainDeparture") { 
-    var countDownDate = new Date("Jul 28, 2023 13:12:00 UTC+0").getTime(); 
-  } else if (countdownElement === "trainArrival") {
-    var countDownDate = new Date("Jul 29, 2023 20:56:00 UTC+0").getTime();
-  } else if (countdownElement === "taxiDeparture") {
-    var countDownDate = new Date("Jul 28, 2023 12:12:00 UTC+0").getTime();
-  } else if (countdownElement === "taxiArrival") {
-    var countDownDate = new Date("Jul 29, 2023 21:56:00 UTC+0").getTime();
-  } else if (countdownElement === "start") {
-    var countDownDate = new Date("2023-07-17T19:00:00Z").getTime();
-  } else if (countdownElement === "test") {
-    var countDownDate = new Date("2023-07-17T21:00:00Z").getTime();
-  }
+  const countDownDate = timers[countdownElement]
   // TODO: Make automatic highlight of the smallest countdown
 
   // Update the count down every 1 second
@@ -54,6 +51,8 @@ function writeTimer(countdownElement) {
 
     // Display the result in the element
     // TODO: Make adding elements entirely script-based
+    // TODO?: Related, make elements into iframes, 
+    // so that the selection of the timer text persists
     document.getElementById(countdownElement).textContent = timeInDHMS;
     try {
       document.getElementById(countdownElement + "Seconds").textContent = timeInSeconds;
@@ -64,7 +63,6 @@ function writeTimer(countdownElement) {
   }, 1000);
 }
 
-const dhms = {'days': true, 'hours': true, 'minutes': true, 'seconds': true}
 document.getElementById("test").textContent = "AAAAAAAAAAAAAAAAA";
 writeTimer("taxiDeparture");
 writeTimer("trainDeparture");
