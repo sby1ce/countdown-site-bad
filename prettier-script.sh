1fddf53d62d59f60d9f0c189273ca35290560ec2
SomeBodY 1BD <someonecallsyou@gmail.com>
2023-10-06 01:28:23 +0300

    Uuh, deleted

--- prettier-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npx prettier --write scripts/main.mjs
-npx prettier --write scripts/storageModule.mjs
-npx prettier --write scripts/timersModule.mjs
-npx prettier --write __tests__/timers.test.js
