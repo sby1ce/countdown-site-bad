3ba48a1ecf416efa020034b2f2b29ae526c0025a
SomeBodY 1BD <someonecallsyou@gmail.com>
2023-10-17 22:04:28 +0300

    Giving up on clipboard buttons for now

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import FooterButton from "./FooterButton.svelte";
-  import { timers } from "./timers.ts";
 
   function deleteLocalStorage() {
     if (localStorage.length) {
@@ -10,34 +9,10 @@
       console.log("localStorage already clear");
     }
   }
-
-  function timersToClipboard() {
-    navigator.clipboard.writeText(`timers${JSON.stringify($timers)}`);
-  }
-
-  function clipboardToTimers() {
-    navigator.clipboard.readText().then((clipText) => {
-      const timersString: string = clipText.slice(6);
-
-      if (
-        clipText === null ||
-        (!clipText.startsWith("timers") || timersString.length < 2)
-      ) {
-        throw new Error('Incorrect JSON in clipboard');
-      }
-
-      timers.set(JSON.parse(timersString));
-    }).catch(error => { throw new Error(error); });
-  }
 </script>
 
 <footer>
   <FooterButton on:click={deleteLocalStorage}>Delete localStorage</FooterButton>
-
-  <FooterButton on:click={timersToClipboard}>Timers to clipboard</FooterButton>
-
-  <FooterButton on:click={clipboardToTimers}>Timers from clipboard</FooterButton
-  >
 </footer>
 
 <style>
