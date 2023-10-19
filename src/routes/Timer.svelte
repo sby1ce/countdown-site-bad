cc01261fc95a0f6bbf48134c586e9b77390112f0
SomeBodY 1BD <someonecallsyou@gmail.com>
2023-10-20 00:08:13 +0300

    Popout window

--- src/routes/Timer.svelte
+++ src/routes/Timer.svelte
@@ -2,6 +2,31 @@
   import { timers } from "./timers.ts";
 
   export let position: number;
+
+  let top: string;
+  let left: string = "90%";
+  
+  type Symbol = '-' | 'v';
+  let symbol: Symbol = '-';
+  let hidden: boolean = true;
+
+  function settings(e: Event) {
+    e.preventDefault();
+
+    if (symbol === '-') {
+      const button = (e.currentTarget as HTMLButtonElement)!;
+      const buttonRect = button.getBoundingClientRect();
+
+      top = `${buttonRect.top + window.scrollY}px`;
+      left = `${buttonRect.left + window.scrollX + button.offsetWidth}px`
+
+      symbol = 'v';
+      hidden = false;
+    } else {
+      symbol = '-';
+      hidden = true;
+    }
+  }
 </script>
 
 <article>
@@ -10,6 +35,15 @@
     {#each $timers[position].timerStrings as countdown}
       <p>{countdown}</p>
     {/each}
+    
+    <button class="settings" type="button" on:click={settings}>
+      {symbol}
+    </button>
+
+    <div class:hidden style:left style:top>
+      <button>gaming</button>
+      <button>gaming 2</button>
+    </div>
   </section>
 </article>
 
@@ -31,5 +65,30 @@
   p {
     /*box-sizing: border-box;*/
     margin: 0px 5px 0px 5px;
+  }
+
+  .settings {
+    font-family: inherit;
+    box-sizing: border-box;
+    inline-size: 0.5em;
+    min-block-size: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div {
+    display: flex;
+    border-width: 2em;
+    border-color: whitesmoke;
+    border-style: solid;
+    position: absolute;
+    z-index: 255;
+    flex-direction: column;
+  }
+
+  .hidden {
+    display: none;
   }
 </style>
